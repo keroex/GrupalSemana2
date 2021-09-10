27a8e5039142db76e0d40480d595aab924d150ad
keroex <kerox@gmail.com>
2021-09-10 20:14:07 -0300

    lazaro se la re re come

--- app/src/main/java/com/utec/grupalsemana2/MostraActividadDeCampo.java
+++ app/src/main/java/com/utec/grupalsemana2/MostraActividadDeCampo.java
@@ -3,18 +3,12 @@ package com.utec.grupalsemana2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class AltaActividadDeCampo extends AppCompatActivity {
+public class MostraActividadDeCampo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alta_actividad_de_campo);
-
-    }
-
-    public void CargarActividadDeCampo(View view) {
-
+        setContentView(R.layout.activity_mostra_actividad_de_campo);
     }
 }
--- app/src/main/java/logica/ActividadDeCampo.java
+++ app/src/main/java/logica/ActividadDeCampo.java
@@ -1,3 +1,5 @@
+package logica;
+
 public class ActividadDeCampo {
 
     //Atributos
