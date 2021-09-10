59fec5eabc67849ff48ba73b5eb1628cd6b8c55a
keroex <kerox@gmail.com>
2021-09-10 19:04:20 -0300

    lazaro se la commit

---
+++ app/src/main/java/com/utec/grupalsemana2/AltaActividadDeCampo.java
@@ -0,0 +1,14 @@
+package com.utec.grupalsemana2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AltaActividadDeCampo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_actividad_de_campo);
+    }
+}
