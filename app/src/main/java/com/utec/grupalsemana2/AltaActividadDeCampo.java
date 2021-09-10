3da839f237f4c82bfcbbb110d646d184343aa993
keroex <kerox@gmail.com>
2021-09-10 19:31:12 -0300

    lazaro se la re commit

--- app/src/main/java/com/utec/grupalsemana2/AltaActividadDeCampo.java
+++ app/src/main/java/com/utec/grupalsemana2/AltaActividadDeCampo.java
@@ -10,5 +10,6 @@ public class AltaActividadDeCampo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alta_actividad_de_campo);
+
     }
 }
